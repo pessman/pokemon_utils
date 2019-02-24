11b0a88f52d632f4f92038887acce8dd1db85410
Peter Essman <pessman.dev@gmail.com>
2019-02-24 11:56:14 -0500

    updated model options to accept data

--- pokemon/models.py
+++ pokemon/models.py
@@ -1,39 +1,48 @@
 from django.db import models
 
 class Ability(models.Model):
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, unique=True)
     description = models.CharField(max_length=256)
 
     class Meta:
         verbose_name = 'ability'
         verbose_name_plural = 'abilities'
 
+    def __str__(self):
+        return str(self.name)
+
 class Item(models.Model):
     ITEM_CATEGORY_CHOICES = (
-        ('BATTLE_ITEMS', 'Battle Items'),
+        ('BATTLE ITEMS', 'Battle Items'),
         ('BERRIES', 'Berries'),
-        ('GENERAL_ITEMS', 'General Items'),
-        ('HOLD_ITEMS', 'Hold Items'),
+        ('GENERAL ITEMS', 'General Items'),
+        ('HOLD ITEMS', 'Hold Items'),
         ('MACHINES', 'Machines'),
         ('MEDICINE', 'Medicine'),
         ('POKEBALLS', 'Pokeballs'),
         ('UNKOWN', 'Uknown')
     )
-    name = models.CharField(max_length=32)
-    category = models.CharField(max_length=16, choices=ITEM_CATEGORY_CHOICES)
-    effect = models.CharField(max_length=256)
+    name = models.CharField(max_length=32, unique=True)
+    category = models.CharField(max_length=16, null=True, choices=ITEM_CATEGORY_CHOICES)
+    effect = models.CharField(max_length=256, null=True)
 
     class Meta:
         verbose_name = 'item'
         verbose_name_plural = 'items'
 
+    def __str__(self):
+        return str(self.name)
+
 class Type(models.Model):
-    name = models.CharField(max_length=32)
+    name = models.CharField(max_length=32, unique=True)
     description = models.CharField(max_length=512)
 
     class Meta:
         verbose_name = 'type'
         verbose_name_plural = 'types'
+
+    def __str__(self):
+        return str(self.name)
 
 class Move(models.Model):
     MOVE_CATEGORY_CHOICES = (
@@ -41,23 +50,27 @@ class Move(models.Model):
         ('SPECIAL', 'Special'),
         ('STATUS', 'Status')
     )
-    name = models.CharField(max_length=64)
-    type = models.OneToOneField(Type, null=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=64, unique=True)
+    type = models.ForeignKey(Type, null=True, on_delete=models.SET_NULL)
     category = models.CharField(max_length=16, choices=MOVE_CATEGORY_CHOICES, null=True)
     power = models.IntegerField(null=True)
     accuracy = models.IntegerField(null=True)
     power_points = models.IntegerField()
     tm = models.CharField(max_length=8, null=True)
-    effect = models.CharField(max_length=256)
+    effect = models.CharField(max_length=256, null=True)
+    effect_percent_chance = models.IntegerField(null=True)
 
     class Meta:
         verbose_name = 'move'
         verbose_name_plural = 'moves'
 
+    def __str__(self):
+        return str(self.name)
+
 class Pokemon(models.Model):
     pokedex = models.IntegerField()
     name = models.CharField(max_length=32)
-    form = models.CharField(max_length=32)
+    form = models.CharField(max_length=32, null=True)
     types = models.ManyToManyField(Type, related_name='pokemon', related_query_name='pokemon')
     moves = models.ManyToManyField(Move, related_name='pokemon', related_query_name='pokemon')
     abilities = models.ManyToManyField(Ability, related_name='pokemon', related_query_name='pokemon')
@@ -71,3 +84,6 @@ class Pokemon(models.Model):
     class Meta:
         verbose_name = 'pokemon'
         verbose_name_plural = 'pokemon'
+
+    def __str__(self):
+        return "{}: {}".format(self.pokedex, self.name)
