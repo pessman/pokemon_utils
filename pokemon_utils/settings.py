eaa83ed57710c628c3d72db297da807c0c6d4b90
Peter Essman <pessman.dev@gmail.com>
2019-02-24 19:06:28 -0500

    reverted to swagger 2.1.2 and added custom swagger settings to project

--- pokemon_utils/settings.py
+++ pokemon_utils/settings.py
@@ -133,3 +133,15 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'api_key': {
+            'type': 'apiKey',
+            'in': 'header',
+            'name': 'Authorization'
+        }
+    },
+    'USE_SESSION_AUTH': False,
+    'JSON_EDITOR': True,
+}
