9b8c1611e3be1350c7a913a877a6f6be99ccbd0d
Peter Essman <pessman.dev@gmail.com>
2019-02-24 19:06:03 -0500

    enabled IsAdminUser permissions on views

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 from pokemon.models import Ability, Item, Move, Pokemon, Type
 from pokemon.pagination import PokemonPagination
@@ -11,23 +12,31 @@ class AbilityViewSet(viewsets.ModelViewSet):
     queryset = Ability.objects.all()
     serializer_class = AbilitySerializer
     pagination_class = PokemonPagination
+    permission_classes = (IsAdminUser,)
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     pagination_class = PokemonPagination
+    permission_classes = (IsAdminUser,)
+
 
 class MoveViewSet(viewsets.ModelViewSet):
     queryset = Move.objects.all()
     serializer_class = MoveSerializer
     pagination_class = PokemonPagination
+    permission_classes = (IsAdminUser,)
+
 
 class PokemonViewSet(viewsets.ModelViewSet):
     queryset = Pokemon.objects.all()
     serializer_class = PokemonSerializer
     pagination_class = PokemonPagination
+    permission_classes = (IsAdminUser,)
+
 
 class TypeViewSet(viewsets.ModelViewSet):
     queryset = Type.objects.all()
     serializer_class = TypeSerializer
     pagination_class = PokemonPagination
+    permission_classes = (IsAdminUser,)
