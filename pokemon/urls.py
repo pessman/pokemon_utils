37d5200b7ba803fc21886aa077c699bec34249c6
Peter Essman <pessman.dev@gmail.com>
2019-02-25 22:12:53 -0500

    url comment update

--- pokemon/urls.py
+++ pokemon/urls.py
@@ -1,4 +1,4 @@
-"""pokemon_utils URL Configuration
+"""pokemon URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
