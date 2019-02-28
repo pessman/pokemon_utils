fe39ecea19519d5a7645bcd9350fbcc1434d7a86
Peter Essman <pessman.dev@gmail.com>
2019-02-27 21:33:03 -0500

    custom permission class now being used on all pokemon app endpoints

--- pokemon/views.py
+++ pokemon/views.py
@@ -1,10 +1,10 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from pokemon.filters import (AbilityFilter, ItemFilter, MoveFilter,
                              PokemonFilter, TypeFilter)
 from pokemon.models import Ability, Item, Move, Pokemon, Type
 from pokemon.pagination import PokemonPagination
+from pokemon.permissions import IsAdminOrReadOnly
 from pokemon.serializers import (AbilitySerializer, ItemSerializer,
                                  MoveSerializer, PokemonSerializer,
                                  TypeSerializer)
@@ -14,7 +14,7 @@ class AbilityViewSet(viewsets.ModelViewSet):
     queryset = Ability.objects.all()
     serializer_class = AbilitySerializer
     pagination_class = PokemonPagination
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnly,)
     filterset_class = AbilityFilter
 
 
@@ -22,7 +22,7 @@ class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     pagination_class = PokemonPagination
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnly,)
     filterset_class = ItemFilter
 
 
@@ -30,7 +30,7 @@ class MoveViewSet(viewsets.ModelViewSet):
     queryset = Move.objects.all()
     serializer_class = MoveSerializer
     pagination_class = PokemonPagination
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnly,)
     filterset_class = MoveFilter
 
 
@@ -38,7 +38,7 @@ class PokemonViewSet(viewsets.ModelViewSet):
     queryset = Pokemon.objects.all()
     serializer_class = PokemonSerializer
     pagination_class = PokemonPagination
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnly,)
     filterset_class = PokemonFilter
 
 
@@ -46,5 +46,5 @@ class TypeViewSet(viewsets.ModelViewSet):
     queryset = Type.objects.all()
     serializer_class = TypeSerializer
     pagination_class = PokemonPagination
-    permission_classes = (IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsAdminOrReadOnly,)
     filterset_class = TypeFilter
