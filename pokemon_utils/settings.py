ac4a708d5c93e93e993a523cf8d1709dd730e223
Peter Essman <pessman.dev@gmail.com>
2019-02-28 17:15:54 -0500

    fixed settings imports

--- pokemon_utils/settings.py
+++ pokemon_utils/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from dj_database_url
+
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -173,7 +174,8 @@ SWAGGER_SETTINGS = {
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-LOGIN_REDIRECT_URL = '/pokemon/info/' # change this to Angular front end url later on
+# change this to Angular front end url later on
+LOGIN_REDIRECT_URL = '/pokemon/info/'
 
 DATABASES['default'] = dj_database_url.config(
     conn_max_age=600, ssl_require=True)
